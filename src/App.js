af07c59ac601747efc9505fe50f9e6d35890c7dd
Hayuna <marek.kowalonek@gmail.com>
2020-07-05 13:55:59 +0200

    Add preview functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css'
 
 const App = () => {
+  const [markdown, setMarkdown] = useState('')
   return (
     <div className="App">
-      <div className="editor"></div>
-      <div className="preview"></div>
+      <div className="editor">
+        <textarea onChange={(e) => setMarkdown(e.target.value)}></textarea>
+      </div>
+      <div className="preview">{markdown}</div>
     </div>
   );
 }
