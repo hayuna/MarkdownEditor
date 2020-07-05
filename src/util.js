7259f222a9b640592642fcfecda51320e27f95af
Hayuna <marek.kowalonek@gmail.com>
2020-07-05 13:57:24 +0200

    Add h2 functionality

--- src/util.js
+++ src/util.js
@@ -1,5 +1,6 @@
 import React from 'react'
 export const pattern = (line) => {
     if (/^# (.+)/.test(line)) return <h1>{line.match(/^# (.+)/)?.[1]}</h1>
+    if (/^## (.+)/.test(line)) return <h2>{line.match(/^## (.+)/)?.[1]}</h2>
     else return <p>{line}</p>
 }
