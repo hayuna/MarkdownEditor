c2877a17bff3a4eb2a10da0a5c521f1f11446dd1
Hayuna <marek.kowalonek@gmail.com>
2020-07-05 13:57:38 +0200

    Add h3 functionality

--- src/util.js
+++ src/util.js
@@ -2,5 +2,6 @@ import React from 'react'
 export const pattern = (line) => {
     if (/^# (.+)/.test(line)) return <h1>{line.match(/^# (.+)/)?.[1]}</h1>
     if (/^## (.+)/.test(line)) return <h2>{line.match(/^## (.+)/)?.[1]}</h2>
+    if (/^### (.+)/.test(line)) return <h3>{line.match(/^### (.+)/)?.[1]}</h3>
     else return <p>{line}</p>
 }
