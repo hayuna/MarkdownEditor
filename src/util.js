17fd47aedc3f9fc5c64343feefbe7bacb82ec91d
Hayuna <marek.kowalonek@gmail.com>
2020-07-05 13:58:08 +0200

    Add h5 functionality

--- src/util.js
+++ src/util.js
@@ -4,5 +4,6 @@ export const pattern = (line) => {
     if (/^## (.+)/.test(line)) return <h2>{line.match(/^## (.+)/)?.[1]}</h2>
     if (/^### (.+)/.test(line)) return <h3>{line.match(/^### (.+)/)?.[1]}</h3>
     if (/^#### (.+)/.test(line)) return <h4>{line.match(/^#### (.+)/)?.[1]}</h4>
+    if (/^##### (.+)/.test(line)) return <h5>{line.match(/^##### (.+)/)?.[1]}</h5>
     else return <p>{line}</p>
 }
