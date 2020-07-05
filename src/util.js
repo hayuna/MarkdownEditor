3ea720c3f8dbf2c0d2e63fdbe61ee774cc81c5c0
Hayuna <marek.kowalonek@gmail.com>
2020-07-05 13:57:53 +0200

    Add h4 functionality

--- src/util.js
+++ src/util.js
@@ -3,5 +3,6 @@ export const pattern = (line) => {
     if (/^# (.+)/.test(line)) return <h1>{line.match(/^# (.+)/)?.[1]}</h1>
     if (/^## (.+)/.test(line)) return <h2>{line.match(/^## (.+)/)?.[1]}</h2>
     if (/^### (.+)/.test(line)) return <h3>{line.match(/^### (.+)/)?.[1]}</h3>
+    if (/^#### (.+)/.test(line)) return <h4>{line.match(/^#### (.+)/)?.[1]}</h4>
     else return <p>{line}</p>
 }
